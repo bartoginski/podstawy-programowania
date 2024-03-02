036722fa75228e42b36bbfed2f3f641a9a76a9d6
Bartosz Oginski <bartoginski@outlook.com>
2024-03-02 10:44:20 +0100

    Refactor - move file zajecia to separated directory

---
+++ zajecia1/zadania.md
@@ -0,0 +1,13 @@
+# Zajecia 1
+
+## Zadania
+
+1) Wyświetl wynik dodawania, odejmowania, mnożenia oraz dzielenia 2 wartości.
+2) Stwórz tablicę liczb całkowitych a następnie oblicz średnią wartość.
+3) Wyświetl, czy dana liczba jest parzysta czy nieparzysta.
+4) Napisz program obliczający wskaźnik masy ciała (BMI).
+5) Napisz program, który dla tablicy wyników z kilkoma ocenami obliczy średnią ocen i określi odpowiedni stopień zaliczenia (np. "A", "B", "C" itp.).
+6) Napisz program, który sprawdza, czy dane słowo jest palindromem.
+Link: https://pl.wikipedia.org/wiki/Palindrom
+Michael Dreyer
+exhibition space W.O. Scheibe Museum in Stuttgart, where he organised the "Palindrom". He also exhibited "Oben und Unten mit Rex Whistler &amp; Friends" with Tom
---
+++ zajecia1/zajecia.java
@@ -0,0 +1,107 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Zajecia {
+    public static void main(String[] args) {
+        String x = "hello world";
+        System.out.println(x);
+
+        enum Colors {
+            RED,
+            GREEN,
+            BLUE,
+        }
+
+        Colors myFavColor = Colors.BLUE;
+
+        runAllExercises();
+
+    }
+
+    /**
+     * Runs all exercises methods.
+     */
+    private static void runAllExercises() {
+
+        calculations();
+
+        System.out.println(averageOfNumbers(new int [] { 1,3, 4 }));
+
+        System.out.println(isEven(10));
+
+        System.out.println(calcBMI(1.85, 75));
+
+        System.out.println(calcGrade(new int [] { 1,3, 4 }));
+
+        System.out.println(isPalindrome("kajak"));
+    }
+
+    private static void calculations() {
+        System.out.println(2 + 4);
+        System.out.println(8 - 3);
+        System.out.println(9 * 2);
+        System.out.println(11 / 2);
+    }
+
+    /**
+     * Calculates average of the passed numbers,
+     * @param numbers
+     * @return Average of the numbers;
+     */
+    private static double averageOfNumbers(int[] numbers) {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        double avg = (double) sum / numbers.length;
+
+        return avg;
+    }
+
+
+    /**
+     * Checks if number is even.
+     * @param number number to evaluate.
+     * @return The Boolean value stating whether the specified number is even
+     */
+    private static boolean isEven(int number) {
+
+        return number % 2 == 0;
+    }
+
+    /**
+     * Calculates BMI based on passed params.
+     * @param height in meters
+     * @param weight in kilograms
+     */
+    private static double calcBMI(double height, double weight) {
+        return weight / (height * height);
+    }
+
+
+    /**
+     * Calculates the final grades, rounding the average.
+     * @param grades grades from 1 to 6.
+     * @return The final grade based on average of grades.
+     */
+    private static int calcGrade(int[] grades) {
+        double average = averageOfNumbers(grades);
+
+        return (int) Math.round(average);
+    }
+
+
+    /**
+     * Check if the passed word is a palindrome.
+     * @param word Word to check
+     * @return Boolean value stating whether the specified word is a palindrome.
+     */
+    private static boolean isPalindrome(String word) {
+        StringBuilder stringBuilder = new StringBuilder(word);
+
+        return word.equalsIgnoreCase(stringBuilder.reverse().toString());
+    }
+}
