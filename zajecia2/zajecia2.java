88114dd3dcb8a31c07b10af0ca8f02188cd37285
Bartosz Oginski <bartoginski@outlook.com>
2024-03-03 23:16:44 +0100

    Feature - add an early stage of first task in lab2

--- zajecia2/zajecia2.java
+++ zajecia2/zajecia2.java
@@ -3,5 +3,21 @@ package zajecia2;
 public class zajecia2 {
     public static void main(String[] args) {
         System.out.println("Hello world ");
+
+        System.out.println(findIndexOfElement(new int[] { 4, 32, 3, 5, 12 }, 5));
+    }
+
+    private static int findIndexOfElement(int[] array, int numberToFind) {
+        int indexOfNumber = -1;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (numberToFind == array[i]) {
+                indexOfNumber = i;
+            }
+
+        }
+
+        return indexOfNumber;
     }
 }
